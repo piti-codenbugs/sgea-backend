097e30f071789d194424d766224459aea07275fb
MichaelMarinReyes <michaelmarin201831260@cunoc.edu.gt>
2026-02-21 16:57:28 -0600

    Creación de proyecto en Spring Boot

---
+++ sgea-api/src/main/java/com/codenbugs/sgeaapi/SgeaApiApplication.java
@@ -0,0 +1,13 @@
+package com.codenbugs.sgeaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgeaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgeaApiApplication.class, args);
+	}
+
+}
---
+++ sgea-api/src/test/java/com/codenbugs/sgeaapi/SgeaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codenbugs.sgeaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgeaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
